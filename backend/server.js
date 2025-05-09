fe9b84e7b2deeeefd5286ff6b09b21c184dcb6c1
alphonso7 <neharejayesh@gmail.com>
2025-05-09 19:58:02 +0530

    removed cors

--- backend/server.js
+++ backend/server.js
@@ -25,27 +25,27 @@ const authMiddleware = require("./middleware/authMiddleware");
 
 // const allowedOrigins = ["http://localhost:5000", "http://localhost:4000"];
 
-app.use(cors({
-    origin: function (origin, callback) {
-        if (!origin || allowedOrigins.includes(origin)) {
-            callback(null, origin);
-        } else {
-            callback(new Error("Not allowed by CORS"));
-        }
-    },
-    credentials: true // ✅ REQUIRED for cookies to work
-}));
+// app.use(cors({
+//     origin: function (origin, callback) {
+//         if (!origin || allowedOrigins.includes(origin)) {
+//             callback(null, origin);
+//         } else {
+//             callback(new Error("Not allowed by CORS"));
+//         }
+//     },
+//     credentials: true // ✅ REQUIRED for cookies to work
+// }));
 
-app.use((req, res, next) => {
-    const origin = req.headers.origin;
-    if (allowedOrigins.includes(origin)) {
-        res.header("Access-Control-Allow-Origin", origin);
-        res.header("Access-Control-Allow-Credentials", "true"); // ✅ REQUIRED
-        res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-        res.header("Access-Control-Allow-Headers", "Content-Type, auth-token");
-    }
-    next();
-});
+// app.use((req, res, next) => {
+//     const origin = req.headers.origin;
+//     if (allowedOrigins.includes(origin)) {
+//         res.header("Access-Control-Allow-Origin", origin);
+//         res.header("Access-Control-Allow-Credentials", "true"); // ✅ REQUIRED
+//         res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+//         res.header("Access-Control-Allow-Headers", "Content-Type, auth-token");
+//     }
+//     next();
+// });
 
 // Database connection
 mongoose
